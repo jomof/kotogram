998b047667d76e398ca37706566dd4a5c1d7caa5
Jomo Fisher <jomofisher@gmail.com>
2025-12-18 20:24:48 +0000

    Bump version to 0.0.15

--- kotogram/__init__.py
+++ kotogram/__init__.py
@@ -1,6 +1,6 @@
 """Kotogram - A dual Python/TypeScript library for Japanese text parsing and encoding."""
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 from .japanese_parser import JapaneseParser
 from .sudachi_japanese_parser import SudachiJapaneseParser
