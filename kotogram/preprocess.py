3317f49f3fa92c641efdd12f75b48152aa0fa042
jomofisher <jomofisher@gmail.com>
2025-12-19 19:04:33 -0800

    disable preprocess

--- kotogram/preprocess.py
+++ kotogram/preprocess.py
@@ -15,6 +15,7 @@ def preprocess(text: str, parser: Any = None) -> Tuple[str, List[str]]:
     Returns:
         Tuple of (preprocessed sentence, list of preprocessing types applied)
     """
+    return text, []
     if not text:
         return text, []
         
