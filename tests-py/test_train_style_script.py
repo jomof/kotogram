da2994ac714a6ab26fa8ad3ec48dd7b1ebd3a211
jomofisher <jomofisher@gmail.com>
2026-04-04 07:58:47 -0700

    Fix tests

--- tests-py/test_train_style_script.py
+++ tests-py/test_train_style_script.py
@@ -40,6 +40,7 @@ class TestTrainStyleScript(unittest.TestCase):
             # Verify label phase output files using glob patterns
             expected_label_manifest = [
                 "[.cache]/style_dataset/vocab.json",
+                "[.cache]/style_dataset/content_mask.bin",
                 "[.cache]/style_dataset/sentences.txt",
                 "[.cache]/style_dataset/kotograms.txt",
                 "[.cache]/style_dataset/kc_*.bin",
